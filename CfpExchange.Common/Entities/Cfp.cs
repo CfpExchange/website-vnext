955d107976f9f3fed647d1f99fbeb592edf73e4a
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-02 10:43:15 +0200

    Improved Cfp entity Added EventType enumeration

--- CfpExchange.Common/Entities/Cfp.cs
+++ CfpExchange.Common/Entities/Cfp.cs
@@ -6,31 +6,61 @@ namespace CfpExchange.Common.Entities
 {
 	public class Cfp : TableEntity
 	{
-		public Guid Id { get; set; }
+		[IgnoreProperty]
+		public Guid Id
+		{
+			get => Guid.Parse(RowKey); 
+			set => RowKey = value.ToString(); 
+		}
+
 		public string EventName { get; set; }
+
 		public string EventDescription { get; set; }
+
 		public string EventLocationName { get; set; }
-		public double EventLocationLat { get; set; }
-		public double EventLocationLng { get; set; }
+
+		public double EventLatitude { get; set; }
+
+		public double EventLongitude { get; set; }
+
 		public string EventUrl { get; set; } = string.Empty;
-		public string EventTags { get; set; } = String.Empty;
+
+		public string Tags { get; set; } = string.Empty;
+
 		public string EventImage { get; set; }
+
 		public DateTime EventStartDate { get; set; }
+
 		public DateTime EventEndDate { get; set; }
+
 		public string EventTimezone { get; set; }
-		public DateTime CfpStartDate { get; set; }
-		public DateTime CfpEndDate { get; set; }
-		public DateTime CfpDecisionDate { get; set; }
-		public DateTime CfpAdded { get; set; }
-		public bool? ProvidesAccommodation { get; set; }
-		public bool? ProvidesTravelAssistance { get; set; }
-		public string CfpUrl { get; set; } = string.Empty;
-		public string Remarks { get; set; }
-		public int Views { get; set; }
-		public int ClicksToCfpUrl { get; set; }
-		public string SubmittedByName { get; set; }
+
 		public string EventTwitterHandle { get; set; }
+
+		public DateTime StartDate { get; set; }
+
+		public DateTime EndDate { get; set; }
+
+		public DateTime DecisionDate { get; set; }
+
+		public DateTime DateAdded { get; set; }
+
+		public bool? ProvidesAccommodation { get; set; }
+
+		public bool? ProvidesTravelAssistance { get; set; }
+
+		public string CfpUrl { get; set; } = string.Empty;
+
+		public string Remarks { get; set; }
+
+		public int Views { get; set; }
+
+		public int ClicksToCfpUrl { get; set; }
+
+		public string SubmittedBy { get; set; }
+
 		public string Slug { get; set; }
+
 		public bool IsVirtual { get; set; }
 	}
 }
---
+++ CfpExchange.Common/Enumerations/EventType.cs
@@ -0,0 +1,9 @@
+﻿namespace CfpExchange.Common.Enumerations
+{
+	public enum EventType
+	{
+		InPerson = 1,
+		Hybrid = 2,
+		Online = 4
+	}
+}
