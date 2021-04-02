8e2cd12f390ccc47f029f0bd0d59aad2f02e6080
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-02 10:23:23 +0200

    Implemented TableStorageHelper (gets entities from a CloudTable)

---
+++ CfpExchange.Common/Helpers/TableStorageHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Cosmos.Table;
+
+namespace CfpExchange.Common.Helpers
+{
+	public static class TableStorageHelper
+	{
+		public static async Task<IEnumerable<T>> GetEntitiesFromTableAsync<T>(CloudTable table) where T : ITableEntity, new()
+		{
+			TableQuerySegment<T> querySegment = null;
+			var entities = new List<T>();
+			var query = new TableQuery<T>();
+
+			do
+			{
+				querySegment = await table.ExecuteQuerySegmentedAsync(query, querySegment?.ContinuationToken);
+				entities.AddRange(querySegment.Results);
+			} while (querySegment.ContinuationToken != null);
+
+			return entities;
+		}
+
+		public static async Task<IEnumerable<T>> GetEntitiesFromTableByPartitionKeyAsync<T>(CloudTable table, string partitionKey) where T : ITableEntity, new()
+		{
+			TableQuerySegment<T> querySegment = null;
+			var entities = new List<T>();
+			var query = new TableQuery<T>().Where(TableQuery.GenerateFilterCondition(nameof(ITableEntity.PartitionKey), QueryComparisons.Equal, partitionKey));
+
+			do
+			{
+				querySegment = await table.ExecuteQuerySegmentedAsync(query, querySegment?.ContinuationToken);
+				entities.AddRange(querySegment.Results);
+			} while (querySegment.ContinuationToken != null);
+
+			return entities;
+		}
+	}
+}
