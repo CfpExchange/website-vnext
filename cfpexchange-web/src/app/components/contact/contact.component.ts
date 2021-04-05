2b59729a7c03e04707a16f15b0a869fe80288f5d
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-05 21:15:04 +0200

    Implement sending mail from contact component

--- cfpexchange-web/src/app/components/contact/contact.component.ts
+++ cfpexchange-web/src/app/components/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Contact } from 'src/app/models/contact';
+import { MailService } from 'src/app/services/mail.service';
 
 @Component({
   selector: 'app-contact',
@@ -10,12 +12,15 @@ export class ContactComponent implements OnInit {
 
   public Contact: Contact = new Contact();
 
-  constructor() { }
+  constructor(private mailService: MailService) { }
 
   ngOnInit(): void {
   }
 
   public sendContact(): void {
-    console.log(this.Contact);
+    this.mailService.sendContact(this.Contact).subscribe((data) => {
+      this.Contact = new Contact();
+    }, (err) => {
+    });
   }
 }
