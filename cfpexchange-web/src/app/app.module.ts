a7027f7cf9a3e7ef1cbe4ecbb71f8e6a65ef942b
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-05 12:24:33 +0200

    Initial setup

---
+++ cfpexchange-web/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AboutComponent,
+    AppComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
