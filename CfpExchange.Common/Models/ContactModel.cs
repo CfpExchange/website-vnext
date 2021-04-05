ef470946df24c2e74daf33bae12fa1faaba54bec
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-05 21:14:03 +0200

    ContactModel

---
+++ CfpExchange.Common/Models/ContactModel.cs
@@ -0,0 +1,11 @@
+namespace CfpExchange.Common.Models
+{
+	public class ContactModel
+	{
+		public string Name { get; set; }
+
+		public string Email { get; set; }
+
+		public string Message { get; set; }
+	}
+}
