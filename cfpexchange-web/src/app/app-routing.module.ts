6a61dd5daf84315f1b6907ca4c0ec150dbefd1c6
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-06 22:06:08 +0200

    Routing!

--- cfpexchange-web/src/app/app-routing.module.ts
+++ cfpexchange-web/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './components/about/about.component';
+import { CfpComponent } from './components/cfp/cfp.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [  {
   path: '',
   component: HomeComponent,
+}, {
+  path: 'cfps',
+  component: CfpComponent,
 }, {
   path: 'contact',
   component: ContactComponent,
