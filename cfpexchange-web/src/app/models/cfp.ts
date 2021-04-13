3f66f2c767bc4e7c2f24f895a38b2118a1ecbaa9
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-13 16:57:30 +0200

    Removed original Angular application

--- cfpexchange-web/src/app/models/cfp.ts
+++
@@ -1,5 +0,0 @@
-export class Cfp {
-	public slug: string | undefined;
-	public eventName: string | undefined;
-	public eventImage: string | undefined;
-}
--- cfpexchange-web/src/app/models/contact.ts
+++
@@ -1,5 +0,0 @@
-export class Contact {
-	public name: string | undefined;
-	public email: string | undefined;
-	public message: string | undefined;
-}
